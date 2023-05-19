d581a6b95023a5138de32d407372b41de531e474
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-05-19 12:53:15 +0530

    Adding frontend using Vite@latest

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,21 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      {
+        headers: {
+          "private-key": "4db7b942-2e37-4c3f-aead-ce9055c74535",
+        },
+      }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+
   return res.json({ username: username, secret: "sha256..." });
 });
 // 4db7b942-2e37-4c3f-aead-ce9055c74535
